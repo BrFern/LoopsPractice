6df606b09398c07db5e4bf82d285b4b8fffc33c9
Bailey <baileyrosefern@gmail.com>
2022-12-01 13:27:14 -0700

    Took me a minute to figure out I was just calling a function in question 4. DOH!

--- loops-2-1-1.js
+++ loops-2-1-1.js
@@ -49,37 +49,43 @@
 
 // Write code here 
 
-for ( let i = 0; i < 20; i++)
-    if (i % 2 == 0) {
-        console.log(`Your number is even. ${i}`)
-    } else {
-        console.log(`Your number is odd ${i}`)
-    }
+// for ( let i = 0; i < 20; i++)
+//     if (i % 2 == 0) {
+//         console.log(`Your number is even. ${i}`)
+//     } else {
+//         console.log(`Your number is odd ${i}`)
+//     }
 
 /*********** Question 3: MULTIPLICATION TABLE ************/
 // Write a for loop that will iterate from 0 to 10. For each iteration of the for loop, it will multiply the number by 9 and log the result (e.g. "2 * 9 = 18").
 // Bonus: Use a nested for loop to show the tables for every multiplier from 1 to 10 (100 results total).
 
 // Write code here 
+// for (let i=0; i<=10; i++) {
+//     const answer = i * 9
+//     console.log(i)
+// }
+    
 
 
 /*********** Question 4: THE GRADE ASSIGNER ************/
 // Check the results of your assignGrade function by logging every value from 60 to 100: your log should show "For 88, you got a B. For 89, you got a B. For 90, you got an A. For 91, you got an A.", etc., logging each grade point in the range.
+// for (let i=60; i <= 100; i++)
+function assignGrade(score) {
+    if (score >= 90) {
+        return 'A';
+    } else if (score >= 80) {
+        return 'B';
+    } else if (score >= 70) {
+        return 'C';
+    } else if (score >= 65) {
+        return 'D';
+    } else {
+        return 'F';
+    }
+}
+console.log(assignGrade(76))
 
-
-// function assignGrade(score) {
-//     if (score > 90) {
-//         return 'A';
-//     } else if (score > 80) {
-//         return 'B';
-//     } else if (score > 70) {
-//         return 'C';
-//     } else if (score > 65) {
-//         return 'D';
-//     } else {
-//         return 'F';
-//     }
-// }
 
 // Write code here 
 
